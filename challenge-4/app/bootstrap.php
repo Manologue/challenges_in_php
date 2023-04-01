c1e927211c4c58536cc82810649e99cb30adf612
moumi manoah <manomoumi@gmail.com>
2023-04-01 19:30:28 +0100

    arranging the boostrap file in challenge 4

--- challenge-4/app/bootstrap.php
+++ challenge-4/app/bootstrap.php
@@ -10,11 +10,6 @@ require APP_ROOT . '/app/functions.php'; // Functions
 require APP_ROOT . '/config/config.php'; // Configuration data
 require APP_ROOT . '/vendor/autoload.php'; // Autoload libraries
 
-if (DEV === false) { // If not in development
-   set_exception_handler('handle_exception'); // Set exception handler
-   set_error_handler('handle_error'); // Set error handler
-   register_shutdown_function('handle_shutdown'); // Set shutdown handler
-}
 
 Session::action(); // Create session
 
